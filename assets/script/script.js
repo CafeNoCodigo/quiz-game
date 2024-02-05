d94ea617f7bdf6803e6af625b125a64dcf417142
FabioMainato <fabiaochirindzamainato@gmail.com>
2024-02-05 08:05:45 +0000

    Quiz Part

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,10 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz');
+const btnClose = document.querySelector('.btn-close');
+const boxHidden = document.querySelector('.box-hidden');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -11,4 +15,17 @@ startBtn.onclick = () => {
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    quizSection.classList.add('active');
+    main.classList.remove('active');
+    boxHidden.classList.add('active');
+
+}
+
+btnClose.onclick = () => {
+    quizSection.classList.remove('active');
+    boxHidden.classList.remove('active');
 }
