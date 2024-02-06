aef3f47722dc8b859bff26123659d70cc7941643
FabioMainato <fabiaochirindzamainato@gmail.com>
2024-02-06 18:04:30 +0000

    Add questions and questio script

---
+++ assets/script/question.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Como se diz 'Óla' em chinês?",
+        answer: "C. 你好",
+        options: [
+            "A. 你呢",
+            "B. 我好",
+            "C. 你好",
+            "D. 你好吗"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Qual dos seguintes caracteres chineses representa 'água'?",
+        answer: "C. 水",
+        options: [
+            "A. 火",
+            "B. 山",
+            "C. 水",
+            "D. 木"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Como se diz 'obrigado' em chinês",
+        answer: "B. 谢谢",
+        options: [
+            "A. 再见",
+            "B. 谢谢",
+            "C. 对不起",
+            "D. 你好吗"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Qual é o número correto para 'dez' em chinês?",
+        answer: "B. 十",
+        options: [
+            "A. 五",
+            "B. 十",
+            "C. 百",
+            "D. 三"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Qual é a tradução correta para 'eu gosto de comer maçãs' em chinês?",
+        answer: "A. 我喜欢吃苹果",
+        options: [
+            "A. 我喜欢吃苹果",
+            "B. 你喜欢吃苹果吗",
+            "C. 我喜欢吃橙子",
+            "D. 你好吗"
+        ]
+    },
+];
--- assets/script/script.js
+++ assets/script/script.js
@@ -6,6 +6,12 @@ const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz');
 const btnClose = document.querySelector('.btn-close');
 const boxHidden = document.querySelector('.box-hidden');
+const sobreBtn = document.querySelector('.sobre-section');
+const sectionSobre = document.querySelector('.sobre');
+const homeBtn = document.querySelector('.home-section');
+const quizBox = document.querySelector('.box');
+const nextBtn = document.querySelector('.btn-next');
+const optionList = document.querySelector('.option-list');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -22,10 +28,76 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     main.classList.remove('active');
     boxHidden.classList.add('active');
-
+    quizBox.classList.add('active');
+    btnClose.classList.add('active');
+    nextBtn.classList.remove('desative');
+    
+    showQuestion(0);
 }
 
 btnClose.onclick = () => {
     quizSection.classList.remove('active');
     boxHidden.classList.remove('active');
+    quizBox.classList.remove('active');
+    btnClose.classList.remove('active');
+    questionCount = 0;
+}
+
+sobreBtn.onclick = () => {
+    sectionSobre.classList.add('active');
+    sobreBtn.classList.add('active');
+    homeBtn.classList.remove('active');
+}
+
+homeBtn.onclick = () => {
+    sectionSobre.classList.remove('active');
+    sobreBtn.classList.remove('active');
+    homeBtn.classList.add('active');
+}
+
+var questionCount = 0;
+
+nextBtn.onclick = () => {
+    questionCount++;
+    if(questionCount <= questions.length - 1){
+        showQuestion(questionCount);
+    }else{
+        nextBtn.classList.add('desative');
+        alert('NÍVEL TERMINADO');
+    }
+    
+    optionList.classList.remove('answerUser');
+    
+}
+
+//Recebe o numero da questão através de um array e mostra essa questão
+function showQuestion(index){
+    const questionTxt = document.querySelector('.question-txt');
+
+    questionTxt.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<p class="option">${questions[index].options[0]}</p>
+                     <p class="option">${questions[index].options[1]}</p>
+                     <p class="option">${questions[index].options[2]}</p>
+                     <p class="option">${questions[index].options[3]}</p>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for(let i = 0; i < option.length; i++){
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    if(userAnswer == correctAnswer){
+        optionList.classList.add('answerUser');
+        answer.classList.add('correct');
+    }else{
+        optionList.classList.add('answerUser');
+        // answer.classList.add('correct');
+        // userAnswer.id = "wrong";
+    }
 }
